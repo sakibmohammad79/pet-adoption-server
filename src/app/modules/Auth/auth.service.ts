8746ad594925d10e8fa1ee564f210530c2938f20
sakib <mohammadsakib7679@gmail.com>
2024-09-17 17:38:31 +0600

    generate access token using jsonwebtoken

--- src/app/modules/Auth/auth.service.ts
+++ src/app/modules/Auth/auth.service.ts
@@ -1,8 +1,38 @@
+import { UserStatus } from "@prisma/client";
+import prisma from "../../../shared/prisma";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
 const loginUserIntoDB = async (payload: {
   password: string;
   email: string;
 }) => {
-  console.log(payload);
+  const user = await prisma.user.findFirstOrThrow({
+    where: {
+      email: payload.email,
+      status: UserStatus.ACTIVE,
+    },
+  });
+
+  const isPasswordCorrect = await bcrypt.compare(
+    payload.password,
+    user.password
+  );
+
+  if (!isPasswordCorrect) {
+    throw new Error("Your password is incorrect!");
+  }
+
+  const jwtPayload = {
+    userId: user.id,
+    email: user.email,
+    role: user.role,
+  };
+  const token = jwt.sign(jwtPayload, "abcde", {
+    algorithm: "HS256",
+    expiresIn: "3d",
+  });
+  console.log(token);
 };
 
 export const AuthService = {
