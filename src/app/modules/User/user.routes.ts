60f308212c382aaa21a81446c5994aaf6f784a5d
sakib <mohammadsakib7679@gmail.com>
2024-09-20 11:33:00 +0600

    image upload successfully using multer

--- src/app/modules/User/user.routes.ts
+++ src/app/modules/User/user.routes.ts
@@ -4,12 +4,14 @@ import { UserValidationSchema } from "./user.validation";
 import { validateRequest } from "../../middleware/validateRequest";
 import { UserRole } from "@prisma/client";
 import Guard from "../../middleware/guard";
+import { imageUploader } from "../../../helpers/imageUploader";
 
 const router = Router();
 
 router.post(
   "/",
   Guard(UserRole.ADMIN),
+  imageUploader.upload.single("file"),
   validateRequest(UserValidationSchema.createAdminValidationSchema),
   UserController.createAdmin
 );
