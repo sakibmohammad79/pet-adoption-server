1f57b31a082beec6df2a1319694f394a3199e36a
sakib <mohammadsakib7679@gmail.com>
2025-05-07 19:48:09 +0600

    create message delete route

--- src/app/modules/Contact/contact.routes.ts
+++ src/app/modules/Contact/contact.routes.ts
@@ -2,6 +2,8 @@ import { Router } from "express";
 import { ContactMessageController } from "./contact.controller";
 import { validateRequest } from "../../middleware/validateRequest";
 import { ContactMessageValidation } from "./contact.validation";
+import Guard from "../../middleware/guard";
+import { UserRole } from "@prisma/client";
 
 const router = Router();
 
@@ -9,5 +11,7 @@ router.get('/', ContactMessageController.getAllContactMessage);
 
 router.post('/create-message', validateRequest(ContactMessageValidation.createContactontactMessageValidationSchema),
      ContactMessageController.createContactMessage);
+
+router.delete('/delete-message', Guard(UserRole.ADMIN), ContactMessageController.deleteContactMessage);
 
 export const ContactMessageRoutes = router;
