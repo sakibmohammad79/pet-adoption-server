41ab65ccaf2c761f935dc62459f4bd2fbc9d2be9
sakib <mohammadsakib7679@gmail.com>
2025-05-07 19:45:34 +0600

    create message delete function

--- src/app/modules/Contact/contact.service.ts
+++ src/app/modules/Contact/contact.service.ts
@@ -7,12 +7,22 @@ const createContactMessgeIntoDB = async(payload: any) => {
  });
  return result;
 }
+
 const getAllContactMessgeFromDB = async() => {
  const result = await prisma.contactMessage.findMany();
  return result;
 }
 
+const deleteMessageFromDB = async (id: string) => {
+    const result = await prisma.contactMessage.delete({
+        where: {
+            id
+        }
+    })
+}
+
 export const ContactMessageService = {
     createContactMessgeIntoDB,
     getAllContactMessgeFromDB,
+    deleteMessageFromDB
 }
